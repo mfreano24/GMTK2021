74f9f588fd47a849100a78ef33d61b955d96889d
Michael Freaney <michael.freaney324@gmail.com>
2021-06-11 16:14:01 -0500

    Layers added, not quite finished with the script though

---
+++ Assets/Scripts/Rope/CollisionLayerSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionLayerSwitch : MonoBehaviour
+{
+    [Header("Layers")]
+    public LayerMask ropeLayer;
+    public LayerMask ropeColliderSetA;
+    public LayerMask ropeColliderSetB;
+    public bool ignoreSetAFirst;
+
+    bool isOnLayerA = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            isOnLayerA = true;
+        }
+    }
+
+    private void SwitchToLayerA()
+    {
+        Physics.IgnoreLayerCollision(ropeColliderSetB, ropeLayer, false);
+        Physics.IgnoreLayerCollision(ropeColliderSetA, ropeLayer, true);
+    }
+
+    private void SwitchToLayerB()
+    {
+        Physics.IgnoreLayerCollision(ropeColliderSetA, ropeLayer, false);
+        Physics.IgnoreLayerCollision(ropeColliderSetB, ropeLayer, true);
+    }
+}
+
+
+
+
