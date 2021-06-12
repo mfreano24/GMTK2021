7bef5facc9d89b58e10e5f057a6c9be7ba7857fe
ealexrose <47873095+ealexrose@users.noreply.github.com>
2021-06-11 23:03:17 -0500

    ClimbableLayerCheck

---
+++ Assets/ValidClimbSpotFinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValidClimbSpotFinder : MonoBehaviour
+{
+    public LayerMask climbingLayers;
+    public BoxCollider2D playerCollider;
+
+    public bool CheckPlayerPosition() 
+    {
+        Vector2 boxDimensions = new Vector2(playerCollider.size.x, playerCollider.size.y);
+
+        if (Physics2D.OverlapBox(playerCollider.gameObject.transform.position, boxDimensions, 0f, climbingLayers))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+}
