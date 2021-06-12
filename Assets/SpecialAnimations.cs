fcdca7abfa8562e0410bd5a50df2d2e0489553af
ealexrose <47873095+ealexrose@users.noreply.github.com>
2021-06-12 16:04:45 -0500

    I might break things so I'm pushing this :)

---
+++ Assets/SpecialAnimations.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialAnimations : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+    private Material matWhite;
+    private Material matDefault;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        matWhite = Resources.Load("WhiteFlash", typeof(Material)) as Material;
+        matDefault = spriteRenderer.material;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            TurnWhite();
+        }
+        else 
+        {
+            TurnNormal();
+        }
+    }
+
+    void TurnWhite() 
+    {
+        spriteRenderer.material = matWhite;
+    }
+
+    void TurnNormal() 
+    {
+        spriteRenderer.material = matDefault;
+    }
+}
